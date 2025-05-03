ead54781d04b394715d0d8db3ce19dafdf6a533a
Jon Zaballa Zarzosa <jzaballazarzosa@gmail.com>
2025-05-03 23:40:49 +0200

    feat: add route and api description to ping endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @ApiOkResponse({ type: String, description: 'Pong' })
+  @Get('ping')
   ping(): string {
     return this.appService.ping();
   }
